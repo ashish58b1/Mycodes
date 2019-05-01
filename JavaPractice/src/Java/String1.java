2fd62ef67deef426277829cc364a91d3967d1455
www.ashish06@gmail.com <www.ashish06@gmail.com>
2019-05-01 17:21:01 +0530

    1st May

--- JavaPractice/src/Java/String1.java
+++ JavaPractice/src/Java/String1.java
@@ -1,9 +1,27 @@
 package Java;
 
+import java.util.Scanner;
+
 public class String1 {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello");
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String s= scn.nextLine();
+		String[] s2 = new String[] {s};
+		String s1= new String(s);
+		
+		char c1;
+		
+		char[] c= s.toCharArray();
+		System.out.println(c);
+		for(int i=0; i<s.length(); i++){
+			System.out.println(c[i]);
+		}
+		
+		for(int i=0; i<s2.length; i++){
+			c1=s1.charAt(i);
+			System.out.println("c1 = "+c1);
+		}
 		
 	}
 

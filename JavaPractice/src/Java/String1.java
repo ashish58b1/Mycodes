935e5a82d420ccb0114a72e1d992bc19e885298f
www.ashish06@gmail.com <www.ashish06@gmail.com>
2019-05-02 09:52:56 +0530

    Commit on 2nd may

--- JavaPractice/src/Java/String1.java
+++ JavaPractice/src/Java/String1.java
@@ -3,14 +3,14 @@ package Java;
 import java.util.Scanner;
 
 public class String1 {
+	private static Scanner scn;
+
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+		scn = new Scanner(System.in);
 		System.out.println("Enter the string");
 		String s= scn.nextLine();
-		String[] s2 = new String[] {s};
-		String s1= new String(s);
+		//String[] s2 = new String[] {s};
 		
-		char c1;
 		
 		char[] c= s.toCharArray();
 		System.out.println(c);
@@ -18,10 +18,25 @@ public class String1 {
 			System.out.println(c[i]);
 		}
 		
-		for(int i=0; i<s2.length; i++){
+		char[] c11= s.toCharArray();
+		for(int j=s.length()-1;j>0 ; j--){
+			System.out.println(c11[j]);
+		}
+		
+		System.out.println(c11);
+		
+		System.out.println("---------");
+		for(int i=0; i<s.length(); i++){
+			System.out.println(s.charAt(i));
+		}
+		for(int j=s.length()-1;j>0 ; j--){
+			System.out.println(s.charAt(j));
+		}
+		
+/*		for(int i=0; i<s2.length; i++){
 			c1=s1.charAt(i);
 			System.out.println("c1 = "+c1);
-		}
+		}*/
 		
 	}
 
